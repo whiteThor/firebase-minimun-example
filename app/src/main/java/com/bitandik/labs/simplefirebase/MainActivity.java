7eddb67ecdf0ae40a3c381723a2119007809ac63
ykro <aacs85@gmail.com>
2016-06-14 17:10:33 -0600

    updating to new Firebase

--- app/src/main/java/com/bitandik/labs/simplefirebase/MainActivity.java
+++ app/src/main/java/com/bitandik/labs/simplefirebase/MainActivity.java
@@ -3,52 +3,62 @@ package com.bitandik.labs.simplefirebase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.firebase.client.DataSnapshot;
-import com.firebase.client.Firebase;
-import com.firebase.client.FirebaseError;
-import com.firebase.client.ValueEventListener;
-
-import butterknife.Bind;
-import butterknife.ButterKnife;
-import butterknife.OnClick;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
-    private String FIREBASE_URL = "https://to-do-list-ykro.firebaseio.com/";
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private DatabaseReference databaseReference;
     private String FIREBASE_CHILD = "test";
-    @Bind(R.id.editText) EditText editText;
-    Firebase firebase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ButterKnife.bind(this);
-        Firebase.setAndroidContext(this);
-        firebase = new Firebase(FIREBASE_URL).child(FIREBASE_CHILD);
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        databaseReference = database.getReference(FIREBASE_CHILD);
 
-        firebase.addValueEventListener(new ValueEventListener() {
+        databaseReference.setValue("Hello, World!");
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot snapshot) {
                 if (snapshot.getValue() != null) {
+                    String value = snapshot.getValue(String.class);
                     Toast.makeText(MainActivity.this, snapshot.getValue().toString(), Toast.LENGTH_SHORT).show();
                     Log.e(getLocalClassName(), snapshot.getValue().toString());
+                    Log.d(TAG, "Value is: " + value);
                 }
             }
 
             @Override
-            public void onCancelled(FirebaseError error) {
+            public void onCancelled(DatabaseError error) {
+                Log.w(TAG, "Failed to read value.", error.toException());
             }
         });
-    }
 
-    @OnClick(R.id.button)
-    public void writeToFirebase() {
-        firebase.setValue(editText.getText().toString());
-        editText.setText("");
-    }
 
+        final Button btn = (Button)findViewById(R.id.button);
+        final EditText editText = (EditText)findViewById(R.id.editText);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                DatabaseReference newElementReference = databaseReference.push();
+                newElementReference.setValue(editText.getText().toString());
+                editText.setText("");
+
+            }
+        });
+
+    }
 }
